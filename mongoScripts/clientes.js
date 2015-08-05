785eb59ae3d082643546c2a556773caf4e33069c
Jorge Raudales <jorgraudales1@hotmail.es>
2015-08-04 18:16:39 -0600

    scripts
    cliente, pro, factu

---
+++ mongoScripts/clientes.js
@@ -0,0 +1,27 @@
+use InselDB;
+var cliente={
+  "codigo":1,
+  "nombre":"Alex",
+  "apellido":"Banegas",
+  "usuario":"abanegas",
+  "contrasena":"alex",
+  "correo":"alex@gmail.com",
+  "telefono":97443388,
+  "direccion":"Santa Lucia",
+  "historial":""
+};
+db.clientes.insert(cliente);
+
+use InselDB;
+var cliente={
+  "codigo":2,
+  "nombre":"Jorge",
+  "apellido":"Raudales",
+  "usuario":"jraudales",
+  "contrasena":"terres",
+  "correo":"jraudales@gmail.com",
+  "telefono":99789578,
+  "direccion":"Santa Lucia",
+  "historial":""
+};
+db.clientes.insert(cliente);
